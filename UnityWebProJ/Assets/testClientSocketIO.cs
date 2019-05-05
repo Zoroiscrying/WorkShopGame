d322f2763abeb4b53399901616e6339f93e4af35
Zoroiscrying <zoroiscrying@outlook.com>
2019-05-05 17:47:19 +0800

    bug fixing

---
+++ UnityWebProJ/Assets/testClientSocketIO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using SocketIOClient;
+using System.Net.Sockets;
+using System.Net;
+using UnityEngine;
+
+public class testClientSocketIO : MonoBehaviour
+{
+}
